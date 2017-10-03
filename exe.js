cc40c9eed943b00c3cf49c5dfab20eec9c47a477
Pukanator <Pukanator@localhost.localdomain>
2017-10-03 17:46:39 +0300

    +

--- exe.js
+++ exe.js
@@ -1,14 +1,5 @@
 let fs = require('fs');
 let buf = fs.readFileSync(process.argv[2]);
 let str = buf.toString();
-//let target = ' ';
-
-//lrt pos = 0;
-//while (true) {
- // let foundPos = str.indexOf(target, str);
- // if (foundPos == 0) break;
- // pos = foundPos + 1; // продолжить поиск со следующей
-//}
-//console.log(pos);
-let arr= str.split('\n')
+let arr= str.split('\n');
 console.log(arr.length-1);
